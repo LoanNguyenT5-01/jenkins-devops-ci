004a82d0c551dbee09b47c5a51b37380da8dd27f
loannguyent5 <loannguyent5@nashtechglobal.com>
2024-12-10 13:50:07 +0700

    restore

--- vars/pythonPipelineTemplate.groovy
+++ vars/pythonPipelineTemplate.groovy
@@ -2,33 +2,28 @@ import main.pisharp.*
 
 def call(serviceName) {
     def imageRegistry = "registry.hub.docker.com"
-    def credentialDockerId = "docker-hub"
-    def namespaceRegistry = "LoanNguyenT5-01"
-    def gitopsRepo = 'https://github.com/LoanNguyenT5-01/pisharped-gitops.git'
+    def credentialDockerId = "dockerhub-demo-token"
+    def namespaceRegistry = "sanghvt"
+    def gitopsRepo = 'https://github.com/sangntRND/pisharped-gitops.git'
     def gitopsBranch = 'main'
     def gitCredential = 'github'
     def imageBuildTag = "${imageRegistry}/${namespaceRegistry}/${serviceName}:${BRANCH_NAME}-${BUILD_NUMBER}"
-    def sonarHostURL = 'http://localhost:9000/'
+    def sonarHostURL = 'http://13.213.249.3:9000/'
     def trivy = new Trivy()
     def global = new Global()
     def sonar = new Sonar()
 
-    stage('Prepare Package') {
+    stage ('Prepare Package') {
         script {
-            if (isUnix()) {
-                sh 'mkdir -p .ci'
-            } else {
-                bat 'mkdir .ci'
-            }
-            
+            sh "mkdir -p .ci"
             writeFile file: '.ci/html.tpl', text: libraryResource('trivy/html.tpl')
         }
     }
     // Step 1: Scan all the application to check if we can put any sensitive informations in the source code or not
-    // trivy.trivyScanSecrets()
+    trivy.trivyScanSecrets()
 
     // Step 2: Run the unit test to check function code and show the test result
-    // global.runPythonUnitTest()
+    global.runPythonUnitTest()
     global.processTestResults()
 
     // Step 3: Scan the vulnerabilities of each python dependencies
@@ -38,17 +33,16 @@ def call(serviceName) {
     sonar.sonarQubeAnalysis(serviceName, sonarHostURL)
 
     // Step 5: Install python dependencies
-    // global.pythonRunInstallDependencies()
+    global.pythonRunInstallDependencies()
 
-    // // Step 6: Build docker images with the new tag
-    // global.buildDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
+    // Step 6: Build docker images with the new tag
+    global.buildDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
     
-    // // Step 7: Scan the vulnerabilities of the new image
-    // trivy.trivyScanDockerImages(imageBuildTag)
+    // Step 7: Scan the vulnerabilities of the new image
+    trivy.trivyScanDockerImages(imageBuildTag)
     
-    // // Step 8: Push image to image registry and update the new image tag in the gitops repository
-    // // and then Argocd can sync the new deployment
-    // global.pushDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
-    // global.deployToK8S(gitopsRepo: gitopsRepo, gitopsBranch: gitopsBranch, gitCredential: gitCredential, serviceName: serviceName)
-
+    // Step 8: Push image to image registry and update the new image tag in the gitops repository
+    // and then Argocd can sync the new deployment
+    global.pushDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
+    global.deployToK8S(gitopsRepo: gitopsRepo, gitopsBranch: gitopsBranch, gitCredential: gitCredential, serviceName: serviceName)
 }
