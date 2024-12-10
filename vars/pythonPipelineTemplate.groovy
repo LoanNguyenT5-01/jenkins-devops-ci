2e1f9fb792498016bda61543dd9b2af5da96f84d
loannguyent5 <loannguyent5@nashtechglobal.com>
2024-12-10 13:51:15 +0700

    restore

--- vars/pythonPipelineTemplate.groovy
+++ vars/pythonPipelineTemplate.groovy
@@ -3,12 +3,12 @@ import main.pisharp.*
 def call(serviceName) {
     def imageRegistry = "registry.hub.docker.com"
     def credentialDockerId = "dockerhub-demo-token"
-    def namespaceRegistry = "sanghvt"
-    def gitopsRepo = 'https://github.com/sangntRND/pisharped-gitops.git'
+    def namespaceRegistry = "LoanNguyenT5-01"
+    def gitopsRepo = 'https://github.com/LoanNguyenT5-01/pisharped-gitops.git'
     def gitopsBranch = 'main'
     def gitCredential = 'github'
     def imageBuildTag = "${imageRegistry}/${namespaceRegistry}/${serviceName}:${BRANCH_NAME}-${BUILD_NUMBER}"
-    def sonarHostURL = 'http://13.213.249.3:9000/'
+    def sonarHostURL = 'http://localhost:9000/'
     def trivy = new Trivy()
     def global = new Global()
     def sonar = new Sonar()
@@ -35,14 +35,15 @@ def call(serviceName) {
     // Step 5: Install python dependencies
     global.pythonRunInstallDependencies()
 
-    // Step 6: Build docker images with the new tag
-    global.buildDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
+    // // Step 6: Build docker images with the new tag
+    // global.buildDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
     
-    // Step 7: Scan the vulnerabilities of the new image
-    trivy.trivyScanDockerImages(imageBuildTag)
+    // // Step 7: Scan the vulnerabilities of the new image
+    // trivy.trivyScanDockerImages(imageBuildTag)
     
-    // Step 8: Push image to image registry and update the new image tag in the gitops repository
-    // and then Argocd can sync the new deployment
-    global.pushDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
-    global.deployToK8S(gitopsRepo: gitopsRepo, gitopsBranch: gitopsBranch, gitCredential: gitCredential, serviceName: serviceName)
+    // // Step 8: Push image to image registry and update the new image tag in the gitops repository
+    // // and then Argocd can sync the new deployment
+    // global.pushDockerImages(imageRegistry: imageRegistry, credentialDockerId: credentialDockerId, namespaceRegistry: namespaceRegistry, serviceName: serviceName)
+    // global.deployToK8S(gitopsRepo: gitopsRepo, gitopsBranch: gitopsBranch, gitCredential: gitCredential, serviceName: serviceName)
+
 }
