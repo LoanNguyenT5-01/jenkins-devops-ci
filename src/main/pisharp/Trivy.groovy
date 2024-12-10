477177f509e8dbe0e02dfbb569c889661e3dad7c
loannguyent5 <loannguyent5@nashtechglobal.com>
2024-12-10 13:30:05 +0700

    update sh

--- src/main/pisharp/Trivy.groovy
+++ src/main/pisharp/Trivy.groovy
@@ -3,7 +3,9 @@ package main.pisharp
 def trivyScanSecrets() {
     stage ("Scan Secrets By Trivy ") {
         script {
-            sh "trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html"
+            bat """
+            trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html
+            """
             publishHTML (target : [allowMissing: true,
                 alwaysLinkToLastBuild: true,
                 keepAll: true,
@@ -19,7 +21,9 @@ def trivyScanSecrets() {
 def trivyScanVulnerabilities() {
     stage ("Scan Vulnerabilities By Trivy ") {
         script {
-            sh "trivy fs . --severity HIGH,CRITICAL --scanners vuln --exit-code 0 --format template --template @.ci/html.tpl -o .ci/vulnreport.html"
+            bat """
+            trivy fs . --severity HIGH,CRITICAL --scanners vuln --exit-code 0 --format template --template @.ci/html.tpl -o .ci/vulnreport.html
+            """
             publishHTML (target : [allowMissing: true,
                 alwaysLinkToLastBuild: true,
                 keepAll: true,
@@ -32,9 +36,11 @@ def trivyScanVulnerabilities() {
     }
 }
 
-def trivyScanDockerImages(image){
+def trivyScanDockerImages(image) {
     stage ("Scan Docker Images By Trivy") {
-        sh "trivy image --scanners vuln --exit-code 0 --severity HIGH,CRITICAL --format template --template @.ci/html.tpl -o .ci/imagesreport.html ${image}"
+        bat """
+        trivy image --scanners vuln --exit-code 0 --severity HIGH,CRITICAL --format template --template @.ci/html.tpl -o .ci/imagesreport.html ${image}
+        """
         publishHTML (target : [allowMissing: true,
             alwaysLinkToLastBuild: true,
             keepAll: true,
@@ -45,5 +51,3 @@ def trivyScanDockerImages(image){
         )
     }
 }
-
-
